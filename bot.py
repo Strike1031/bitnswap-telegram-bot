41d4cbda889502eb99f4c611749367dc7c8b5e5b
Strike <strikeihaveadream@gmail.com>
2024-03-11 06:04:05 -0400

    Initial commit

---
+++ bot.py
@@ -0,0 +1,221 @@
+# import below, to work Telegram Bot with Django Rest Framework properly
+# start
+import sys
+import time
+
+sys.dont_write_bytecode = True
+
+import os
+from dotenv import load_dotenv
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
+import django
+django.setup()
+
+from app import models, serializers
+
+from asgiref.sync import sync_to_async
+# end
+
+import logging
+from datetime import datetime
+import pytz
+from config import settings
+import pandas
+
+load_dotenv()
+
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Update)
+
+from telegram.ext import (
+    Application,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+)
+import platform
+import asyncio
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+# States
+START_STATE, END_STATE = range(2)
+
+# Callback data
+PLUS, MINUS = range(2)
+
+# File path
+FILE_PATH = 'file/report.xlsx'
+
+TELEGRAM_BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+@sync_to_async
+def post_person(user):
+    models.Person(
+        tg_id=user.id,
+        tg_username=user.username,
+        tg_fullname=user.full_name,
+        arrived_at=get_time(),
+    ).save()
+
+
+@sync_to_async
+def put_person(user, user_id):
+    models.Person.objects.select_related().filter(pk=user_id, tg_id=user.id).update(left_at=get_time())
+
+
+@sync_to_async
+def get_last_id(user):
+    last_id = models.Person.objects.select_related() \
+        .filter(tg_id=user.id, left_at=None).values_list("pk", flat=True).last()
+    active_id = models.Person.objects.select_related() \
+        .filter(tg_id=user.id).values_list("pk", flat=True).last()
+    if last_id >= active_id:
+        return last_id
+    else:
+        return False
+
+
+@sync_to_async
+def get_data():
+    persons = models.Person.objects.all()
+    serializer = serializers.PersonSerializer(persons, many=True)
+
+    all_data = []
+    for i in range(0, len(serializer.data)):
+        data = [serializer.data[i]['tg_fullname'], serializer.data[i]['arrived_at'], serializer.data[i]['left_at']]
+        all_data.append(data)
+
+    return all_data
+
+
+def set_data(info):
+    pandas.DataFrame(data=info, columns=['name', 'arrived', 'left']).to_excel(FILE_PATH)
+    return 1
+
+
+def get_time():
+    current_time = datetime.now(pytz.timezone(settings.TIME_ZONE))
+    return current_time.strftime('%Y-%m-%d %H:%M:%S')
+
+
+async def report(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    active_data = await get_data()
+
+    if set_data(active_data):
+        await update.message.reply_document(
+            document=open(FILE_PATH, 'rb'),
+            filename='report.xlsx',
+            caption='Report'
+        )
+
+    time.sleep(2)
+    os.remove(FILE_PATH)
+
+    return END_STATE
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Send message on `/start`."""
+
+    # Get user that sent /start and log his name
+    user = update.effective_user
+    logger.info("User %s started the conversation.", user.username)
+
+    keyboard = [
+        [
+            InlineKeyboardButton("+", callback_data=str(PLUS)),
+            InlineKeyboardButton("-", callback_data=str(MINUS)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Choose an option", reply_markup=reply_markup,)
+
+    return START_STATE
+
+
+async def plus(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Show confirm button"""
+
+    user = update.effective_user
+    query = update.callback_query
+    await query.answer(text='Saved')
+
+    message = await query.edit_message_text(text=".")
+    await context.bot.delete_message(message.chat.id, message.message_id)
+
+    await post_person(user)
+
+    return END_STATE
+
+
+async def minus(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Show confirm button"""
+
+    user = update.effective_user
+    active_user_id = await get_last_id(user)
+
+    if active_user_id:
+        query = update.callback_query
+        await query.answer(text="Saved")
+
+        message = await query.edit_message_text(text=".")
+        await context.bot.delete_message(message.chat.id, message.message_id)
+
+        print('-'*50)
+        print('active_user_id:', active_user_id)
+        await put_person(user, active_user_id)
+
+        return END_STATE
+    else:
+        query = update.callback_query
+        await query.answer(text="+ then -")
+
+
+async def end(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Returns `ConversationHandler.END`, which tells the
+    ConversationHandler that the conversation is over.
+    """
+    return ConversationHandler.END
+
+
+def main():
+    """Run the bot."""
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+
+    conv_handler = ConversationHandler(
+        entry_points=[
+            CommandHandler("start", start),
+            CommandHandler('report', report)],
+        states={
+            START_STATE: [
+                CallbackQueryHandler(plus, pattern="^" + str(PLUS) + "$"),
+                CallbackQueryHandler(minus, pattern="^" + str(MINUS) + "$"),
+            ],
+            END_STATE: [
+                CallbackQueryHandler(end),
+            ],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+    application.add_handler(conv_handler)
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    main()
